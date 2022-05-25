33c0b6d1e3e8df1ad0ec3e01606d8c440d0b3d51
Timemanchik <timemanchik@mail.ru>
2022-05-25 03:49:32 +0300

    off default belongs to validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+Rails.application.config.active_record.belongs_to_required_by_default = false
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
