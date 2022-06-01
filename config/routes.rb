4a8b7eff5e37455e24aa511d394946fb57899f78
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-06-02 02:11:49 +0300

    fix: fix rout

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  get 'users/:id/matches', to: 'users#matches', as: 'users_matches'
-  delete "attachments/:id/purge", to: "attachments#purge" , as: "purge_attachments"
 
+  delete "attachments/:id/purge", to: "attachments#purge" , as: "purge_attachments"
+  get 'users/:id/matches', to: 'users#matches', as: 'users_matches'
   get 'like/:liker_id/:likee_id', to: 'likes#create', as: 'create_like'
 end
