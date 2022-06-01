83cfd7bba7fb09e7cfbc31921fbfbde4443b62b2
Timemanchik <timemanchik@mail.ru>
2022-06-01 19:18:07 +0300

    add LikesController

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   resources :users
   delete "attachments/:id/purge", to: "attachments#purge" , as: "purge_attachments"
+
+  post 'likes/index', to: 'likes#create', as: 'create_like'
+  get 'likes/index', to: 'likes#index', as: 'index_likes'
 end
