bbba24ee6e8eeccfea02bd1cb93ec61d1523f40f
mgst-d <and8511@ya.ru>
2022-06-03 17:30:23 +0300

    add private room

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,19 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1 or /users/1.json
-  def show; end
+  def show
+    @user = User.find(params[:id])
+    @current_user = current_user
+    @rooms = Room.public_rooms
+    @users = User.all_except(@current_user)
+    @room = Room.new
+    @message = Message.new
+    @room_name = get_name(@user, @current_user)
+    @single_room = Room.where(name: @room_name).first || Room.create_private_room([@user, @current_user], @room_name)
+    @messages = @single_room.messages
+
+    render "rooms/index"
+  end
 
   # GET /users/new
   def new
@@ -74,6 +86,7 @@ class UsersController < ApplicationController
                                  foto: [])
   end
 
+<<<<<<< HEAD
   def users_id_list_is_nil?
     !current_user.nil? && session[:users_id].nil?
   end
@@ -89,5 +102,10 @@ class UsersController < ApplicationController
 
   def reset_session_users_id_list_after_a_period_of_time(time)
     session[:users_id] = nil if current_user.nil? || (Time.now.to_i - session[:date].to_i) > time
+=======
+  def get_name(user1, user2)
+    users = [user1, user2].sort
+    "private_#{users[0].id}_#{users[1].id}"
+>>>>>>> add private room
   end
 end
