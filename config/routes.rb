5e6fb0acf027a573827030eb9d37f2627a33855d
Timemanchik <timemanchik@mail.ru>
2022-06-06 22:36:13 +0300

    correct style

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
     end
   end
 
-  delete "attachments/:id/purge", to: "attachments#purge" , as: "purge_attachments"
+  delete 'attachments/:id/purge', to: 'attachments#purge' , as: 'purge_attachments'
   delete 'dislike/:liker_id/:likee_id', to: 'likes#destroy', as: 'destroy_like'
   post 'like/', to: 'likes#create', as: 'create_like'
 end
