dc556e8a61c28583cb555b50736a8dcf0cfac892
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-06-08 00:12:04 +0300

    feat: spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) {described_class.new(first_name: "Kristy", last_name: "ZZZ", birth: "1990-01-01", sex: "Female", yourself: "Hi", latitude: 27.53037290421605, longitude: 53.905427341494146, email: "11@11", work_id: 17)}
+  let(:user) {described_class.new(first_name: "Kristy", last_name: "ZZZ", birth: "1990-01-01", sex: "Female", yourself: "fuck", latitude: 27.53037290421605, longitude: 53.905427341494146, email: "11@11", work_id: 17)}
 
   it 'returns correct name' do
     expect(user.first_name).to eq('Kristy')
@@ -10,4 +10,9 @@ RSpec.describe User, type: :model do
   it 'returns type string' do
     expect(user.first_name).to be_an_instance_of(String)
   end
+
+  it 'returns error' do
+    expect(user.yourself).to eq('?')
+  end
+
 end
