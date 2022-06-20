b6ab7bfa8c8157e6301d7840834e8b61d3fe9703
Timemanchik <timemanchik@mail.ru>
2022-06-21 00:00:12 +0300

    devise email notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_03_133825) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_13_162223) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -111,6 +111,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_03_133825) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.bigint "work_id"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["work_id"], name: "index_users_on_work_id"
