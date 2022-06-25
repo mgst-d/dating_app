1fdaae49f4d97988b1229727b5f98d3f1cc38b18
Timemanchik <timemanchik@mail.ru>
2022-06-25 21:37:48 +0300

    correct spaces2

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "/likes", type: :request do
 
-  let(:photo_with_valid_attributes) { ActiveStorage::Blob.new(filename: 'foto', checksum: 'sadasd', content_type: 'image/jpeg', byte_size: 299946)}
+  let(:photo_with_valid_attributes) { ActiveStorage::Blob.new(filename: 'foto', checksum: 'sadasd', 
+                                                              content_type: 'image/jpeg', byte_size: 299946)}
   let(:user_with_valid_attributes) { User.new(first_name: 'Alex', birth: Date.new(1990,01,01), sex: 'Man', 
                                      latitude: 27.53037290421605, longitude: 53.905427341494146, 
                                      email: 'Timom@mail.ru', password: 'dsdfss') }
-	let(:user2_with_valid_attributes) { User.new(first_name: 'Dasha', birth: Date.new(1990,01,01), sex: 'Female', 
-																			latitude: 27.53037290421605, longitude: 53.905427341494146, 
-																			email: 'Timomyy@mail.ru', password: 'dsdfss') }
+  let(:user2_with_valid_attributes) { User.new(first_name: 'Dasha', birth: Date.new(1990,01,01), sex: 'Female', 
+                                      latitude: 27.53037290421605, longitude: 53.905427341494146, 
+                                      email: 'Timomyy@mail.ru', password: 'dsdfss') }
   before(:each) do
     create_user(user_with_valid_attributes, photo_with_valid_attributes)
     create_user(user2_with_valid_attributes, photo_with_valid_attributes)
@@ -27,7 +28,7 @@ RSpec.describe "/likes", type: :request do
       expect(response).to redirect_to(root_path)
     end
   end
-	
+  
   describe "DELETE /destroy" do
 
     it "destroys like" do
